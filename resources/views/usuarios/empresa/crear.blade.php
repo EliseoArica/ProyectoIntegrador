c2522b68eb3dd4dc4c76625e65e81edba375f234
EliseoArica <eliseo989665091@gmail.com>
2019-12-08 07:29:58 -0500

    Ayuda man, voy a dormir unas horas
    Ya perdí la fe en el proyecto men :/

--- resources/views/usuarios/empresa/crear.blade.php
+++ resources/views/usuarios/empresa/crear.blade.php
@@ -17,12 +17,12 @@
 <div class="editar">
     <div class="container">
         <hr>
-        <form>
+        <form  >
 
             <div class="form-group row color py-5 my-5 justify-content-center align-items-centers">
                 <label for="titulo" class="col-sm-5 col-form-label mb-2">Titulo del empleo</label>
                 <div class="col-sm-6">
-                    <input type="email" class="form-control" id="titulo" placeholder="¿Qué puesto esta solicitando?">
+                    <input type="email" class="form-control" name="title" placeholder="¿Qué puesto esta solicitando?" required>
                 </div>
             </div>
 
@@ -39,35 +39,45 @@
             </div>
 
             <div class="form-group row color py-4 my-5 justify-content-center align-items-center">
-                <label for="descripcion" class="col-sm-5 col-form-label mb-2">Descripción de la empresa</label>
+                <label for="descripcion" class="col-sm-5 col-form-label mb-2">Descripción de la oferta</label>
                 <div class="col-sm-6">
-                    <textarea [froalaEditor]="froala_options" type="text" class="form-control expandir" id="descripcion"></textarea>
+                    <textarea [froalaEditor]="froala_options" type="text" name="description" class="form-control expandir ckeditor" id="description" required></textarea>
                 </div>
             </div>
 
             <div class="form-group row color2 py-4 my-5 justify-content-center align-items-center">
                 <label for="funciones" class="col-sm-5 col-form-label mb-2">Funciones del Cargo</label>
                 <div class="col-sm-6">
-                    <textarea [froalaEditor]="froala_options" type="text" class="form-control expandir" id="funciones"></textarea>
+                    <textarea [froalaEditor]="froala_options" type="text" name="functions" class="form-control expandir ckeditor" id="functions" required></textarea>
                 </div>
             </div>
 
             <div class="form-group row color py-4 my-5 justify-content-center align-items-center">
                 <label for="requerimientos" class="col-sm-5 col-form-label mb-2">Requerimientos del Cargo</label>
                 <div class="col-sm-6">
-                    <textarea [froalaEditor]="froala_options" type="text" class="form-control expandir" id="requerimientos"></textarea>
+                    <textarea [froalaEditor]="froala_options" type="text" name="requirements" class="form-control expandir ckeditor" id="requirements" required></textarea>
                 </div>
             </div>
 
             <div class="form-group row color2 py-5 my-5 justify-content-center align-items-center">
                 <label for="sueldo" class="col-sm-5 col-form-label mb-2">Sueldo Mensual</label>
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" id="sueldo" placeholder="Perú">
+                    <input type="text" class="form-control" id="salary" name="salary" placeholder="S/. " required>
+                </div>
+            </div>
+
+            <div class="form-group row color py-5 my-5 justify-content-center align-items-centers">
+                <label for="sueldo" class="col-sm-5 col-form-label mb-2">Dirección</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" id="address" required>
                 </div>
             </div>
 
             <div class="text-center completar my-5">
-                <a href="#" class="btn btn-primary">Crear publicación</a>
+                <button class="btn btn-primary">
+                    Publicar oferta
+                </button>
+                <!---<a href="#" class="btn btn-primary">Crear publicación</a>--->
             </div>
 
         </form>
