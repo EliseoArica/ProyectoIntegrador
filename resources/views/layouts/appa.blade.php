9ca6f63f65b573aae6040202330e9d0584e545be
Khail Mogollon <khail.mogollon@tecsup.edu.pe>
2019-12-08 00:18:30 -0500

    Pull diminuto como tu cosa :v
    Esa vaina salir sesión del admin

--- resources/views/layouts/appa.blade.php
+++ resources/views/layouts/appa.blade.php
@@ -49,7 +49,19 @@
               </ul>
             </li>
             <li><a href="{{route('admin_estadistica')}}"> <i class="fa fa-bar-chart"></i>Estadisticas                             </a></li>
-            <li><a href="{{route('postulantes')}}"> <i class="fa fa-sign-out"></i>Salir                             </a></li>
+            <li>
+              <a class="dropdown-item" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                              <i class="fa fa-sign-out"></i>
+                  {{ __('Cerrar sesión') }}
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+            </li>
+          
           </ul>
         </div>
 
